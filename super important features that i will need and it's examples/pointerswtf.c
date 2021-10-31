70b4c123d5a4f6734659f817cbb61d324e26a4d5
lapatiy <lany.pusik@gmail.com>
2021-10-31 16:13:59 +0100

    Will understand this later...

---
+++ super important features that i will need and it's examples/pointerswtf.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+// guys explain pointers pls what the fuck
+// i understand that
+int age = 5;
+int *pAge = &age;
+
+//printf("%d\n", age );       -> 5
+//printf("%p\n", age );       -> 00000005
+
+//printf("%d\n", &age );      -> 6422296
+//printf("%p\n", &age );      -> 0061FF18
+
+//printf("%d\n", *&age );     -> 5
+//printf("%p\n", *&age );     -> 00000005
+
+//printf("%d\n", pAge );      -> 6422296
+//printf("%p\n", pAge );      -> 0061FF18
+
+//printf("%d\n", *pAge );     -> 5
+//printf("%p\n", *pAge );     -> 00000005
+
+//printf("%d\n", *&pAge );    -> 6422300         ???
+//printf("%p\n", *&pAge );    -> 0061FF1C        ???
+
+
+  return 0;
+}
