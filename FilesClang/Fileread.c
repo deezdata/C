86688432ff93a428c4ff46557e6f9dbd9f7073e1
lapatiy <lany.pusik@gmail.com>
2021-11-10 18:26:50 +0100

    adding comments to previous file prikoly

--- FilesClang/Fileread.c
+++ FilesClang/Fileread.c
@@ -4,11 +4,12 @@
 int main()
 {
 
-
+  // it's opening a txt file and making an array of char variables
   FILE * fPointer;
   fPointer = fopen("ForReading.txt", "r");
   char line[150];
 
+  //reading all lines in txt file
   while (!feof(fPointer))
   {
     fgets(line, 150, fPointer);
--- FilesClang/Filewrite.c
+++ FilesClang/Filewrite.c
@@ -4,10 +4,10 @@
 int main()
 {
   int age = 18;
-
+  //creating file
   FILE * fPointer2;
   fPointer2 = fopen("ForWriting.txt", "w");
-
+  //writing inside this file
   fprintf(fPointer2, "I am %d years old and I Love my girlfriend Liza\n", age);
   fclose(fPointer2);
 
